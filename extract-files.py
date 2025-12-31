ec107be8048c966a7679db38d0fc738d3c1bb6b7
Pranav Vashi <neobuddy89@gmail.com>
2025-12-31 07:39:49 +0530

    avalon: Extract only from device path
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -88,7 +88,5 @@ module = ExtractUtilsModule(
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(
-        module, 'sm8650-common', module.vendor
-    )
+    utils = ExtractUtils.device(module)
     utils.run()
